9f3813ec81071e39deadf25bb6510739f50fc1c2
XCR-hub <tcerda@xcr.fr>
2025-06-22 13:21:07 -0700

    feat: cache assets in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,62 @@
+const CACHE_NAME = 'xcr-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/vite.svg',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+];
+
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)),
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name)),
+      ),
+    ),
+  );
   clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((response) => {
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== 'basic'
+          ) {
+            return response;
+          }
+
+          const responseToCache = response.clone();
+          caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.put(event.request, responseToCache));
+
+          return response;
+        })
+        .catch(() => caches.match('/index.html'));
+    }),
+  );
 });
